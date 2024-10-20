24bc09dc28837b04d606168dde223aea6a99d4cb
ivan119 <ivankelava91@gmail.com>
2024-10-20 19:35:46 +0200

    feat: setup proxy on server chore: change api

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -14,7 +14,7 @@ const state = reactive({
 })
 onMounted(async () => {
   try {
-    const response = await axios.get('http://localhost:5000/jobs')
+    const response = await axios.get('/api/jobs')
     state.jobs = response.data
   } catch (e) {
     console.log('Error fetching jobs', e)
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3003,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
   },
   resolve: {
     alias: {
