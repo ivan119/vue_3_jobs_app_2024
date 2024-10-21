d1b1b8b7afc215f485b1b3cfbfce6d0513080ce5
ivan119 <ivankelava91@gmail.com>
2024-10-21 17:47:22 +0200

    feat: vue-toastification

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
@@ -7,4 +8,5 @@ import App from './App.vue'
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast)
 app.mount('#app')
